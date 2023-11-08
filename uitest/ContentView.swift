7d925dd2e2b238d088697dc60bd1346879a76819
Alexandra Lopezgurria <a.lopezgurria@gmail.com>
2023-11-08 12:10:26 +0100

    Initial Commit

---
+++ uitest/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  uitest
+//
+//  Created by Alexandra LOPEZ-GURRIA on 08/11/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ uitest/uitestApp.swift
@@ -0,0 +1,17 @@
+//
+//  uitestApp.swift
+//  uitest
+//
+//  Created by Alexandra LOPEZ-GURRIA on 08/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct uitestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
